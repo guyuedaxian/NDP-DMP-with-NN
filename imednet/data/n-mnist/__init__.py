9d37477c13ffa47aa77960aa24723f87ae8ba196
ShengDaolin <15900859362@163.com>
2021-08-24 23:47:40 +0100

    update the architectures of models

---
+++ imednet/data/n-mnist/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Time        : 2021/8/23 2:39 下午
+# @Author      : linksdl
+# @ProjectName : acs-project-msc_project_codes
+# @File        : __init__.py.py
